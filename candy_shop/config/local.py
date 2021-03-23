4085cd6bb87d3a64bd74ce1cbadc8fad20be884d
Tsanda Artyom <tsanda.artyom@gmail.com>
2021-03-23 21:50:59 +0300

    (feat) settings were split in prod and local

---
+++ candy_shop/config/local.py
@@ -0,0 +1,29 @@
+import os
+from .common import Common
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Local(Common):
+    DEBUG = True
+
+    SECRET_KEY = 'q65e6)4@%ah=n-#(6^0g=p-a+3ffs#28s=l4nda-dufv=)x^95'
+
+    # Testing
+    INSTALLED_APPS = Common.INSTALLED_APPS
+
+    # Mail
+    EMAIL_HOST = 'localhost'
+    EMAIL_PORT = 1025
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'candy_shop',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
+
